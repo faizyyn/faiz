38a984a6a6f8803108eaad61a7693373ee5dc82f
faizyyn <143859250+faizyyn@users.noreply.github.com>
2023-09-02 10:44:28 +0700

    First Commit

---
+++ h1.py
@@ -0,0 +1,30 @@
+apel = 52,2
+apel2 = 52
+print(apel)
+print(type(apel))
+print(521)
+print(type(apel2))
+nama_apel = "malang"
+print(nama_apel)
+print(type(nama_apel))
+
+apel_benar = True
+apel_salah = False
+print(type(apel_benar))
+print(apel_salah)
+
+list_warna_apel = ['hijau', 'merah', 'kuning']
+list_apel1 = ['manis , 2 , 3.0']
+print(list_warna_apel)
+print(list_apel1)
+
+tuple_jumlah = (2, 5,25, "satu")
+print(tuple_jumlah)
+
+apel_dictionary = "{'nama': 'apel', 'asal', 'jumlah'}"
+print(apel_dictionary('asal'))
+
+print(34+2)
+print(39-4)
+print(2*6)
+print(9/3)
